a4b10f6ad3b1b1a859ca4e3f19b005baf14ef132
ThalesIFSP <thales.m@aluno.ifsp.edu.br>
2022-05-27 21:08:46 -0300

    Video 19 finish

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -5,8 +5,13 @@
 
     <div id="event-create-container" class="col-md-6 offset-md-3">
         <h1>Crie o seu evento</h1>
-        <form action="/events" method="POST">
+        <form action="/events" method="POST" enctype="multipart/form-data">
             @csrf
+            <div class="form-group">
+                <label for="image">Imagem do Evento: </label>
+                <input type="file" id="image" name="image" class="from-control-file">
+            </div>
+
             <div class="form-group">
                 <label for="title">Evento: </label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Evento">
@@ -28,6 +33,25 @@
             <div class="form-group">
                 <label for="description">Descrição: </label>
                 <textarea name="description" id="description" class="form-control" placeholder="O que acontecerá no evento?"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="description">Adicionar itens de infraestrutura: </label>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Palco"> Palco
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Cerveja grátis"> Cerveja grátis
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Open Food"> Open Food
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="Brindes"> Brindes
+                </div>
+                
             </div>
 
             <input type="submit" class="btn btn-primary" value="Criar Evento">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,29 +3,29 @@
 @section('title', 'HDC Events')
 @section('content')
 
-    <div id="search-container" class="col-md-12">
-        <h1>Busque um evento</h1>
-        <form action="">
-            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
-        </form>
-    </div>
-    <div id="events-container" class="col-md-12">
-        <h2>Próximos Eventos</h2>
-        <p class="subtitle">Veja os eventos dos próximos dias</p>
-        <div id="cards-container" class="row">
-            @foreach ($events as $event)
-                <div class="card col-md-3">
-                    <img src="/img/placeholder-image.png" alt="{{ $event->title }}">
-                    <div class="card-body">
-                        <p class="card-date">27/05/2022</p>
-                        <h5 class="card-title">{{ $event->title }}</h5>
-                        <p class="card-participations">X Participantes</p>
-                        <a href="#" class="btn btn-primary">Saber Mais</a>
-                    </div>
-                </div>
-            @endforeach
-
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach ($events as $event)
+        <div class="card col-md-3">
+            <img src="/img/events/{{$event->image}}" alt="{{ $event->title }}">
+            <div class="card-body">
+                <p class="card-date">27/05/2022</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-participations">X Participantes</p>
+                <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber Mais</a>
+            </div>
         </div>
+        @endforeach
+
     </div>
+</div>
 
 @endsection
